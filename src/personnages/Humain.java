a3704648b549683ea0ed885b82eb8e7e894ddcc9
trilles bastien <trb3539a@ens.fsi>
2022-10-27 10:55:00 +0200

    création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,43 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int argent;
+	
+	public Humain(String nom, String boisson, int argent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("(" + this.nom + ") - " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour! Je m'appelle " + this.nom + " et j'aime boire du " + this.boisson + ".");
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + this.boisson + "! GLOUPS!");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if(prix <= this.argent) {
+			parler("J'ai " + this.argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+			this.argent -= prix;
+		}
+		else {
+			parler("Je n'ai plus que " + this.argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
+		}
+	}
+}
