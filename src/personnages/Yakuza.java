26f7b42beb2747b3185600ddcd08373e0b1beb1d
trilles bastien <trb3539a@ens.fsi>
2022-11-15 09:18:22 +0100

    le clan des Yakuzas

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -35,4 +35,10 @@ public class Yakuza extends Humain{
 		this.reputation ++;
 		parler("Ce ronin pensait vraiment battre " + getNom() + " du clan de " + this.clan + "? Je l'ai dépouillé de ses " + gain + " sous.");
 	}
+	
+	@Override
+	public void direBonjour() {
+		super.direBonjour();
+		parler("Mon clan est celui de " + this.clan + ".");
+	}
 }
